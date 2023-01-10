90790b6ca398766e3861b868e7d348bcb8582111
Ahmed Saad <ahmed.saad.kader@gmail.com>
2023-01-10 20:46:51 +0200

    sharp and stream error handling

--- src/controllers/convertController.ts
+++ src/controllers/convertController.ts
@@ -6,9 +6,17 @@ export const resizeImage = async (req:express.Request, res:express.Response) => 
   const {width, height} = req.query
   // const readStream = fs.createReadStream('./images/full/encenadaport.jpg')
   // const resizedImage = 
-  await sharp('./images/full/encenadaport.jpg').resize(500,500).toFile('./images/resized/output.jpg')
-  const imageStream = fs.createReadStream('./images/resized/output.jpg')
-  res.type('image/jpg')
-  // return readStream.pipe(resizedImage)
-  imageStream.pipe(res)
+  try {
+    await sharp('./images/full/encenadaports.jpg').resize(500,500).toFile('./images/resized/output.jpg')
+    const imageStream = fs.createReadStream('./images/resized/outputs.jpg')
+    imageStream.on('error', () => {
+      res.sendStatus(404).send("image not found")
+    })
+    res.type('image/jpg')
+    // return readStream.pipe(resizedImage)
+    imageStream.pipe(res)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(404).send("image not found")
+  }
 };
